51559b84307356e2844d5c2f01e5f7a9a0fc6a64
Lucas Gabriel Romano <lucasgralcantara@gmail.com>
2020-08-25 19:57:20 -0300

    Funcionalidade: Questão 2-1 implementada. Criada uma lista de objetos chamada correntistas que contém informações dos correntistas.

--- Aula19.js
+++ Aula19.js
@@ -1,3 +1,13 @@
 const helpers = require('./helpers');
+const { codigoBanco, transformaNumeroEmAgencia } = require('./helpers');
 
-console.log(helpers.codigoBanco('001'));
+const correntistas = [
+    {
+        nomeCorrentista: 'Joãozinho do Teste',
+        cpfCorrentista: '12345678912',
+        codigoBanco: '001',
+        agencia: '00123',
+        contaCorrente: '023145',
+        saldo: '0'
+    }
+]
